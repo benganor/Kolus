40a834117714fe463d7713c87d6964e58d3b9127
benganor <bbbwein@gmail.com>
2024-03-01 12:41:55 +0200

    further updates in translation

--- Python/Kolus.py
+++ Python/Kolus.py
@@ -1,23 +1,24 @@
 from K_config import *
 import sys
 #import pyvisa
-#from PyQt5 import QtWidgets 
+from PyQt5 import QtWidgets, QtCore 
 
-def kolus_gen_fig():
-    app = QtWidgets.QApplication(sys.argv)
-    window = QtWidgets.QMainWindow()
-    window.show()  
-    app.exec()
+#def kolus_gen_fig():
+#    app = QtWidgets.QApplication(sys.argv)
+#    window = QtWidgets.QMainWindow()
+#    window.show()  
+#    app.exec()
 
-def kolus_initDAQ():
-    rm = pyvisa.ResourceManager()
+#def kolus_initDAQ():
+#    rm = pyvisa.ResourceManager()
 
 if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv) 
     if not sys.argv:  # No command-line argument
         path = QtWidgets.QFileDialog.getExistingDirectory()
-        QtWidgets.QDir.setCurrent(path)
+        QtCore.QDir.setCurrent(path) 
     else:
-        QtWidgets.QDir.setCurrent(sys.argv[1])
+        QtCore.QDir.setCurrent(sys.argv[1])
 
-    kolus_gen_fig() 
-    kolus_initDAQ()
+#    kolus_gen_fig() 
+#    kolus_initDAQ()
